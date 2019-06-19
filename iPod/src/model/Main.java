9509ba9da7afd3a553d91e28dcbd82c3c6f3be3d
Matt <MattSmelser2007@gmail.com>
2019-06-19 11:15:23 -0500

    finished refactoring iPod, functions and lists items correctly with Hashsets now instead of LinkedLists.

--- iPod/src/model/Main.java
+++ iPod/src/model/Main.java
@@ -6,20 +6,31 @@ import model.media.*;
 
 public class Main {
 
-    private static iPod nano = new iPod("John");
-    private static Movie m1 = new Movie("Dunkirk (2017)", "Christopher Nolan", "Drama",  107);
-    private static Movie m2 = new Movie("Dunkirk (2017)", "Christopher Nolan", "Drama",  107);
-    private static Movie m3 = new Movie("Dunkirk (2017)", "Christopher Nolan", "Drama",  107);
-    private static Song s1 = new Song("Writing's on the Wall", "Sam Smith", "Ballad", 3.02);
-    private static Song s2 = new Song("Writing's on the Wall", "Sam Smith", "Ballad", 3.02);
-    private static Song s3 = new Song("Writing's on the Wall", "Sam Smith", "Ballad", 3.02);
-    private static Photo p1 = new Photo("Moonrise", "Ansel Adams", "Landscape", "Yosemite");
-    private static Photo p2 = new Photo("Moonrise", "Ansel Adams", "Landscape", "Yosemite");
-    private static Photo p3 = new Photo("Moonrise", "Ansel Adams", "Landscape", "Yosemite");
-
     public static void main(String[] args) {
+        iPod nano = new iPod("John");
+        Movie m1 = new Movie("Dunkirk (2017)", "Christopher Nolan", "Drama",  107);
+        Movie m2 = new Movie("The Dark Knight (2008)", "Christopher Nolan", "Action",  107);
+        Movie m3 = new Movie("The Matrix (1999)", "The Wachowski Brothers", "Action",  107);
+        Song s1 = new Song("Writing's on the Wall", "Sam Smith", "Ballad", 3.02);
+        Song s2 = new Song("The Entertainer", "Billy Joel", "Ballad", 3.02);
+        Song s3 = new Song("American Pie", "Don McLean", "Ballad", 7.02);
+        Photo p1 = new Photo("Moonrise", "Ansel Adams", "Landscape", "Yosemite");
+        Photo p2 = new Photo("Sunset", "Jacob Smith", "Landscape", "Hawaii");
+        Photo p3 = new Photo("Earthrise", "Niel Armstrong", "Landscape", "Moon");
+
+
 
        // TODO: add the static objects you've declared before this method to the iPod (nano)
+        nano.addMovie(m1);
+        nano.addMovie(m2);
+        nano.addMovie(m3);
+        nano.addSong(s1);
+        nano.addSong(s2);
+        nano.addSong(s3);
+        nano.addPhoto(p1);
+        nano.addPhoto(p2);
+        nano.addPhoto(p3);
+
 
         System.out.println(nano.getName() +"'s iPod contains: ");
         // TODO: Use the Iterator Design Pattern to make the following for-each loop run
--- iPod/src/model/iPod.java
+++ iPod/src/model/iPod.java
@@ -1,34 +1,59 @@
 package model;
 
-import java.util.LinkedList;
+import model.media.AbstractMedia;
+import model.media.Movie;
+import model.media.Photo;
+import model.media.Song;
+import java.util.*;
 
-public class iPod {
+public class iPod implements Iterable<AbstractMedia>{
 
     private String name;
-    private String movie;
-    private String photo;
-    private String song;
+    private Set<AbstractMedia> songs = new HashSet<>();
+    private Set<AbstractMedia> movies = new HashSet<>();
+    private Collection<AbstractMedia> photos = new HashSet<>();
 
-    LinkedList<String> songs = new LinkedList<>();
-    LinkedList<String> movies = new LinkedList<>();
-    LinkedList<String> photos = new LinkedList<>();
 
     // TODO: add fields here which represent this iPod's Movies, Photos, and Songs, they should be of the Collection type
 
     public iPod(String name) {
         this.name = name;
-        this.movie = movie;
-        this.photo = photo;
-        this.song = song;
     }
 
     // getters
     public String getName() { return name; }
 
-    public String getMovie() {return movie;}
+    public void addMovie(Movie m){movies.add(m);}
+    public void addSong(Song s){songs.add(s);}
+    public void addPhoto(Photo p){photos.add(p);}
 
-    public String getPhoto() {return photo;}
+    @Override
+    public Iterator<AbstractMedia> iterator(){
+        return new AbstractMediaIterator();
+    }
 
-    public String getSong() {return song;}
+    private class AbstractMediaIterator implements Iterator<AbstractMedia>{
+        Iterator movieIterator = movies.iterator();
+        Iterator photoIterator = photos.iterator();
+        Iterator songIterator = songs.iterator();
+
+        @Override
+        public boolean hasNext(){
+            return movieIterator.hasNext()||photoIterator.hasNext()||songIterator.hasNext();
+        }
+
+        @Override
+        public AbstractMedia next(){
+            AbstractMedia m = null;
+            if (movieIterator.hasNext()){
+                m = (Movie) movieIterator.next();
+            } else if (photoIterator.hasNext()){
+                m = (Photo) photoIterator.next();
+            }else if (songIterator.hasNext()){
+                m = (Song) songIterator.next();
+            }
+            return m;
+        }
+    }
 
 }
